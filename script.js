9de8f05d3d25742ed732b04d685ad1c694c47925
tug11537 <tug11537@temple.edu>
2025-02-09 15:24:01 -0500

    updated hw 4
    created script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+let myLetVariable = "Hello";
+const myConstVariable = 42;
+
+
+let myArray = [10, "banana", true];
+myArray.push([1, 2, 3]);
+myArray.push("added string");
+myArray.shift();
+myArray[1] = 345;
+myArray[myArray.length - 1] = false;
+
+let i = 0;
+while (i < myArray.length) {
+    console.log(myArray[i]);
+    i++;
+}
+
+i = 0;
+while (i < myArray.length) {
+    console.log(typeof myArray[i]);
+    i++;
+}
+
+let myScale = ["C", "D", "E", "F", "G", "A", "B"];
+
+i = 0;
+while (i < myScale.length) {
+    console.log(typeof myScale[i]);
+    i++;
+}
+console.log(myScale);
+i = 0;
+while (i < myScale.length) {
+    if (myScale[i] === "F") {
+        myScale[i] = "F#";
+    } else if (myScale[i] === "B") {
+        myScale[i] = "Bb";
+    }
+    i++;
+}
+console.log("Modified myScale array:", myScale);
